848b558ae2a0a4aa336e0d7498adcbda12031820
Shaked Amar <shakedamar@gmail.com>
2018-01-04 17:18:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-DEVICE_ID = '58ad93e7cacfd21b8c1b82f1'
-PRODUCT_ID = '58ad93e7cacfd21b8c1b82f0'
-APP_ID = 'a1b06db6-174a-4ecd-8b68-c204ae395a9b'
-HUB_KEY = '66/pYJm8KGBQ73N6VRf9WSW+ERP3bHt0suyCmxIrja8='
+DEVICE_ID = "<Your Device ID>"
+PRODUCT_ID = "<Your Product ID>"
+APP_ID = "<Your APP ID>"
+HUB_KEY = "<Your Hub Key>"
